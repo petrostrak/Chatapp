ce23e0ca1de5c5e07a3f3a1ef689522508483746
petros <pit.trak@gmail.com>
2020-09-05 15:05:16 +0300

    Added Chatmessage model

---
+++ src/main/java/com/petros/chatapp/model/ChatMessage.java
@@ -0,0 +1,44 @@
+package com.petros.chatapp.model;
+
+/*
+ * ChatMessage model is the message payload that will 
+ * be exchanged between the clients and the server
+ * */
+
+public class ChatMessage {
+	private MessageType type;
+	private String content;
+	private String sender;
+	
+	public enum MessageType {
+		CHAT,
+		JOIN,
+		LEAVE
+	}
+
+	public MessageType getType() {
+		return type;
+	}
+
+	public void setType(MessageType type) {
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	
+	
+}
